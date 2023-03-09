cb4cb75bb360399f0bb2df5e88e79ad577d7228f
“ssohye” <ssohye@icloud.com>
2023-03-09 22:28:00 +0900

    Initial Commit

---
+++ ssopa/ssopaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ssopaApp.swift
+//  ssopa
+//
+//  Created by Jiho Seo on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct ssopaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
