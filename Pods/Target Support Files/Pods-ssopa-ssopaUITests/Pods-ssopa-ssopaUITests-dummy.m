1e66ea3b92ed8c7051b4fe18dc0d175cd0cee520
“ssohye” <ssohye@icloud.com>
2023-04-21 23:08:32 +0900

    add

---
+++ Pods/Target Support Files/Pods-ssopa-ssopaUITests/Pods-ssopa-ssopaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ssopa_ssopaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ssopa_ssopaUITests
+@end
---
+++ Pods/Target Support Files/Pods-ssopa/Pods-ssopa-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ssopa : NSObject
+@end
+@implementation PodsDummy_Pods_ssopa
+@end
